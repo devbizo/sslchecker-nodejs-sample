ee9c0b3836500ef17ebdd391281caf8b9558fa98
Natthaphon Chansama <natthaphon.chansama@gmail.com>
2020-10-07 21:31:04 +0700

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const sslChecker = require('ssl-checker').default
+
+const app = express()
+const port = 3000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/layout', express.static('layout'));
+
+app.get('/', async (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/check', async (req, res) => {
+
+  const info = await sslChecker(req.body.domain, { method: "GET", port: 443 });
+  
+  res.send({ domain: req.body.domain, ...info});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
