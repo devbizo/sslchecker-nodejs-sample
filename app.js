f2aa90233203956ba9c21471ec962fb5937358b4
devbizo <info@devbizo.com>
2020-10-07 21:45:38 +0700

    Remove async on index function

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/layout', express.static('layout'));
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 })
 
